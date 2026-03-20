06c442521644d32b27219282b55978e2384df5a6
Michael Van Delft <michael@xo.tc>
2026-03-21 07:19:05 +0800

    Spawn bubbles randomly
    Honestly I'm pretty happy with that

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -1,28 +1,28 @@
 extends Area2D
 
-const speed = 60
-var direction = [-1, 0, 1].pick_random()
-
+var vertical_speed = 60
+var horizonatal_speed = 0
 
 @onready var timer = $Timer
 
+func _ready() -> void:
+	_on_timer_timeout()
 
 func _on_timer_timeout():
-	direction = [-1, 0, 1].pick_random()
+	vertical_speed += randi_range(-10, 30)
+	horizonatal_speed = randi_range(-20, 20)
+	timer.wait_time = randf_range(0.5, 1.5)
 	timer.start()
 
 
 func _process(delta: float) -> void:
-	position.y -= speed * delta
-	position.x += direction * 60 * delta
-	
-
+	position.y -= vertical_speed * delta
+	position.x += horizonatal_speed * delta
 
 
 func _input_event(viewport, event, shape_idx):
 	# Detect if screen is touched or mouse is clicked
 	if event is InputEventMouseButton or event is InputEventScreenTouch:
 		if event.pressed:
-			direction = [-1, 0, 1].pick_random()
 			print("Sprite tapped!")
 			# Add tap logic here
---
+++ scripts/root.gd
@@ -0,0 +1,21 @@
+extends Node2D
+var bubble = preload("res://bubble.tscn")
+@onready var timer = $BubbleSpawnTimer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_bubble_spawn_timer_timeout() -> void:
+	spawn_bubble()
+	
+
+func spawn_bubble() -> void:
+	var new_bubble = bubble.instantiate()
+	new_bubble.position = Vector2(randf_range(100, 980), 1900)
+	add_child(new_bubble)
