f4ca0fb312d291feb39e0ac6d4bf63e17c338cfd
Michael Van Delft <michael@xo.tc>
2026-03-31 20:29:12 +0800

    Trail for touch point

---
+++ scripts/trail.gd
@@ -0,0 +1,24 @@
+# https://www.youtube.com/watch?v=y8bi0_Fggn0
+extends Line2D
+class_name Trails
+
+var queue: Array
+@export var MAX_LENGTH: int
+
+
+func _process(delta: float) -> void:
+	var pos = get_parent().position
+	queue.push_front(pos)
+	
+	if queue.size() > MAX_LENGTH:
+		queue.pop_back()
+	
+	clear_points()
+	
+	for point in queue:
+		add_point(point)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_released():
+			queue.clear()
