2d010fb4e07157314e622d67f45973b8baa38f80
Michael Van Delft <michael@xo.tc>
2026-04-04 20:03:26 +0800

    Consolidate bubble scripts

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var pop_animation: AnimationPlayer = $PopAnimation
+@onready var sprite: Sprite2D = $Sprite2D
 
 
 var vertical_speed = 60
@@ -9,6 +10,18 @@ var horizontal_speed = 0
 @onready var timer = $Timer
 
 func _ready() -> void:
+	# TODO this isn't too bad with 7 textures, but there must be a way to glob these.
+	# also, maybe the different bubbles can work differently, then we can use them for
+	# specific things rather than just random.
+	var bubble_1 = load("res://assets/Bubbles/bubble_1.png")
+	var bubble_2 = load("res://assets/Bubbles/bubble_2.png")
+	var bubble_3 = load("res://assets/Bubbles/bubble_3.png")
+	var bubble_4 = load("res://assets/Bubbles/bubble_4.png")
+	var bubble_5 = load("res://assets/Bubbles/bubble_5.png")
+	var bubble_6 = load("res://assets/Bubbles/bubble_6.png")
+	var bubble_7 = load("res://assets/Bubbles/bubble_7.png")
+	
+	sprite.texture = [bubble_1, bubble_2, bubble_3, bubble_4, bubble_5, bubble_6, bubble_7].pick_random()
 	_on_timer_timeout()
 
 func _on_timer_timeout():
