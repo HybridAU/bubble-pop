186bc8e65749c28c27f3ecfe8562236f6a3e3f05
Michael Van Delft <michael@xo.tc>
2026-04-05 14:43:49 +0800

    Add pause button and menu

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@onready var pause_animation: AnimationPlayer = $CanvasLayer/PauseAnimation
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
+
+
+func resume():
+	pause_animation.play_backwards("blur")
+	canvas_layer.hide()
+	get_tree().paused = false
+	
+	
+func pause():
+	canvas_layer.show()
+	get_tree().paused = true
+	pause_animation.play("blur")
+
+
+func _on_resume_button_pressed() -> void:
+	resume()
+
+func _on_restart_button_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+func _on_quit_button_pressed() -> void:
+	resume()
+	get_tree().change_scene_to_file("res://menus/main_menu.tscn")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	# This feels like a hack, I think I should be using a button here...
+	pause()
