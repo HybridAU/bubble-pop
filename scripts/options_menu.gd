52691282730d6cbef3bd21975049e2c5343e34ae
Michael Van Delft <michael@xo.tc>
2026-04-06 10:28:51 +0800

    Options menu, set state on scene load

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -1,7 +1,17 @@
 extends Control
 
 var master_bus = AudioServer.get_bus_index("Master")
+
 @onready var toggle_sound_label: Label = $VBoxContainer/ToogleSound/ToggleSoundLabel
+@onready var toggle_sound_button: TextureButton = $VBoxContainer/ToogleSound/ToggleSoundButton
+
+func _ready() -> void:
+	if AudioServer.is_bus_mute(master_bus):
+		toggle_sound_button.set_pressed_no_signal(true)
+		toggle_sound_label.text = "Mute"
+	else:
+		toggle_sound_button.set_pressed_no_signal(false)
+		toggle_sound_label.text = "Sound on"
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://menus/main_menu.tscn")
